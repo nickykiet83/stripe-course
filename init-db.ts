57c70d101ce7298c97fb93abda7e021e39851c42
Kiệt Phạm Quang Anh <phamquanganhkiet@gmail.com>
2020-04-03 10:38:44 +1100

    init db

--- init-db.ts
+++ init-db.ts
@@ -1,17 +1,24 @@
-
-import {COURSES, findLessonsForCourse} from './db-data';
-
 import * as firebase from 'firebase';
 
-var config = {
-    // TODO copy-paste here your own config, taken from the Firebase dashboard
+import { COURSES, findLessonsForCourse } from './db-data';
+
+
+const config = {
+  // TODO copy-paste here your own config, taken from the Firebase dashboard
+  apiKey: 'AIzaSyDwpPWvMhNTLDrOzSwTrLgp9ynifpo1Cy0',
+  authDomain: 'stripe-course-7a044.firebaseapp.com',
+  databaseURL: 'https://stripe-course-7a044.firebaseio.com',
+  projectId: 'stripe-course-7a044',
+  storageBucket: 'stripe-course-7a044.appspot.com',
+  messagingSenderId: '327790143856',
+  appId: '1:327790143856:web:1b09f161771a7b21ed47b5'
 };
 
-console.log("Uploading data to the database with the following config:\n");
+console.log('Uploading data to the database with the following config:\n');
 
 console.log(JSON.stringify(config));
 
-console.log("\n\n\n\nMake sure that this is your own database, so that you have write access to it.\n\n\n");
+console.log('\n\n\n\nMake sure that this is your own database, so that you have write access to it.\n\n\n');
 
 firebase.initializeApp(config);
 
@@ -19,24 +26,24 @@ const db = firebase.firestore();
 
 async function uploadData() {
 
-  var batch = db.batch();
+  const batch = db.batch();
 
   const courses = db.collection('courses');
 
 
   Object.values(COURSES)
-    .sort((c1:any, c2:any) => c1.seqNo - c2.seqNo)
-    .forEach(async (course:any) => {
+    .sort((c1: any, c2: any) => c1.seqNo - c2.seqNo)
+    .forEach(async (course: any) => {
 
       const newCourse = removeId(course);
 
       const courseRef = await courses.add(newCourse);
 
-      const lessons = courseRef.collection("lessons");
+      const lessons = courseRef.collection('lessons');
 
       const courseLessons = findLessonsForCourse(course.id);
 
-      //console.log(`Adding ${courseLessons.length} lessons to ${course.description}`);
+      // console.log(`Adding ${courseLessons.length} lessons to ${course.description}`);
 
       courseLessons.forEach(async lesson => {
 
@@ -52,9 +59,9 @@ async function uploadData() {
 }
 
 
-function removeId(data:any) {
+function removeId(data: any) {
 
-  const newData: any = {...data};
+  const newData: any = { ...data };
 
   delete newData.id;
 
@@ -64,18 +71,18 @@ function removeId(data:any) {
 
 uploadData()
   .then(() => {
-    console.log("Writing data, exiting in 10 seconds ...\n\n");
+    console.log('Writing data, exiting in 10 seconds ...\n\n');
 
     setTimeout(() => {
 
-      console.log("\n\n\nData Upload Completed.\n\n\n");
+      console.log('\n\n\nData Upload Completed.\n\n\n');
       process.exit(0);
 
     }, 10000);
 
   })
   .catch(err => {
-    console.log("Data upload failed, reason:", err, '\n\n\n');
+    console.log('Data upload failed, reason:', err, '\n\n\n');
     process.exit(-1);
   });
 
